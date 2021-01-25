341a5e6197627c999068da148b1993d0d27c6817
Michael Jayasuriya <mjayasur@berkeley.edu>
2021-01-24 16:48:37 -0800

    bare bones file upload

--- app/src/components/FileUpload.js
+++ app/src/components/FileUpload.js
@@ -4,24 +4,59 @@ class FileUpload extends React.Component {
   constructor() {
     super();
     this.state = {
-      someKey: 'someValue'
+      selectedFile: null
     };
+  }
+
+
+  onDataChange = event => {
+    this.setState({ selectedFile: event.target.files[0] }); 
+  }
+
+  uploadData = () => {
+    // Create an object of formData 
+    const formData = new FormData(); 
+    
+    // Update the formData object 
+    formData.append( 
+      "anndata", 
+      this.state.selectedFile, 
+      this.state.selectedFile.name 
+    ); 
+    
+    // Details of the uploaded file 
+    console.log(this.state.selectedFile); 
+    
+    // Request made to the backend api 
+    // Send formData object 
+    // fill with flask shit
+
+    
+    // url = ""
+    // axios.post(url, formData); 
   }
 
   render() {
     return (
-        <div className = "container">
-            <div class="data-upload">
-                <input type="file" class="custom-file-input" id="data-input" />
-                <label class="custom-file-label" for="data-input">Upload data file with <code>.h5ad</code> extension</label>
-            </div>
+
+          
+        <div className="container col-5  mt-3">
+
+            <input onChange={this.onDataChange}  type="file" class="custom-file-input" id="data-input"  />
+            <button onClick={this.uploadData} type="button mt-4 " className="btn btn-success text-left">Upload</button>
+
+            <label className="custom-file-label" for="data-input">Upload data file with <code>.h5ad</code> extension</label>
+            
         </div>
+
+
+
     );
   }
 
   componentDidMount() {
     this.setState({
-      someKey: 'otherValue'
+      
     });
   }
 }
