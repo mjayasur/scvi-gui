b329a17c6c7ee684845e4019a59a12d42b0c6269
Kathy Wu <kathywu@berkeley.edu>
2021-01-24 16:48:44 -0800

    select stuff

--- app/src/App.js
+++ app/src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <FileUpload />
+      <SetupAnndata />
     </div>
   );
 }
---
+++ app/src/components/SetupAnndata.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+class SetupAnndata extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      someKey: 'someValue'
+    };
+  }
+
+  render() {
+    return (
+        <div className = "container">
+            <div class="setup-anndata">
+                <label class="setup-anndata">Batch Key</label>
+                <select class="form-control" id="batch-key-select">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                </select>
+                <label class="setup-anndata">Labels Key</label>
+                <select class="form-control" id="labels-key-select">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                </select>
+                <label class="setup-anndata">Layer</label>
+                <select class="form-control" id="layer-select">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                </select>
+                <label class="setup-anndata">Protein Expression OBSM Key</label>
+                <select class="form-control" id="obsm-key-select">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                </select>
+                <label class="setup-anndata">Protein Names UNS Key</label>
+                <select class="form-control" id="uns-key-select">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                </select>
+                <label class="setup-anndata">Categorical Covariate Keys</label>
+                <select multiple class="form-control" id="categorical-covariate-keys-select">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                </select>
+                <label class="setup-anndata">Continuous Covariate Keys</label>
+                <select multiple class="form-control" id="continuous-covariate-keys-select">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                </select>
+                <input class="form-check-input" type="checkbox" value="" id="copy-of-anndata"></input>
+                <label class="setup-anndata">Return a copy of adata?</label>
+            </div>
+        </div>
+    );
+  }
+
+  componentDidMount() {
+    this.setState({
+      someKey: 'otherValue'
+    });
+  }
+}
+
+export default SetupAnndata;
