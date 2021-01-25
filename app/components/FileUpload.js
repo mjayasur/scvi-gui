0d4838912b50e1feea7b0405d7d54d5616170a8d
Michael Jayasuriya <mjayasur@berkeley.edu>
2021-01-24 16:26:09 -0800

    Added 2 skeleton components and bootstrap to index file

--- app/components/FileUpload.js
+++ app/components/FileUpload.js
@@ -1,3 +1,22 @@
 import React from 'react';
 
+class FileUpload extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      someKey: 'someValue'
+    };
+  }
 
+  render() {
+    return ;
+  }
+
+  componentDidMount() {
+    this.setState({
+      someKey: 'otherValue'
+    });
+  }
+}
+
+export default FileUpload;
