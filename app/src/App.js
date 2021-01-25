eb6b3ca727990a59ea8e2bf6e140448bf000cdb2
Michael Jayasuriya <mjayasur@berkeley.edu>
2021-01-24 16:20:21 -0800

    Made skeleton for react app and flask backend.

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+/*
+1) upload anndata 
+2) check boxes, dropdowns to run scvi.data.setup_anndata, like so they can in a graphical way change the arguments to the function
+*/
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
